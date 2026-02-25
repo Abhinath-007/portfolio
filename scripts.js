65c6e6a1f7c026d60c0871c4e73a77b0f701da67
Abhinath-007 <abhinath944744@gmail.com>
2026-02-25 15:48:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert("hallo world")
+function calculate(){
+   const mainTextElement=document.getElementById("main-txt");
+   if(mainTextElement.innerText === "Enter your date of birth")
+   {
+    mainTextElement.innerText= "Your age is";    
+   }
+   else
+   {
+    mainTextElement.innerText= "Enter your date of birth"  ;
+   }
+}
